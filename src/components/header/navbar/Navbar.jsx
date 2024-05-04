ce1d3915f5d02069415009a8e87d9fc76194edb8
Dilshodbek20040104 <dilshoditolmasov32@gmail.com>
2024-05-04 19:02:01 +0500

    admin panel uzgartildi

--- src/components/header/navbar/Navbar.jsx
+++ src/components/header/navbar/Navbar.jsx
@@ -1,52 +1,54 @@
-import React, { memo } from 'react'
-import './Navbar.css'
-import { IoMenu, IoSearch } from "react-icons/io5"
+import React, { memo } from "react";
+import "./Navbar.css";
+import { IoMenu, IoSearch } from "react-icons/io5";
 import { FaRegUser, FaRegHeart } from "react-icons/fa";
 import { IoHome } from "react-icons/io5";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import { FiShoppingCart } from "react-icons/fi";
+import { RiAdminLine } from "react-icons/ri";
 import logo from "../../../assets/images/footerLogo.svg";
 
 function Navbar() {
+  return (
+    <header className="navbar__wrapper">
+      <div className="container">
+        <nav className="navbar">
+          <NavLink to={"/"} className="navbar__logo">
+            <img src={logo} alt="logo" />
+          </NavLink>
+          <button className="navbar__category-btn">
+            <IoMenu />
+            <span>Katalog</span>
+          </button>
+          <div className="navbar__search">
+            <input type="text" placeholder="Search..." />
+            <button>
+              <IoSearch />
+            </button>
+          </div>
+          <div className="navbar__collection">
+            <NavLink to={"/"} className="navbar__item">
+              <IoHome />
+              <span>Bosh Sahifa</span>
+            </NavLink>
+            <NavLink to={"/login"} className="navbar__item">
+              <FaRegUser />
+              <span>Account</span>
+            </NavLink>
+            <NavLink to={"/wishes"} className="navbar__item">
+              <FaRegHeart />
 
-    return (
-        <header className='navbar__wrapper'>
-            <div className='container'>
-                <nav className="navbar">
-                    <NavLink to={'/'} className="navbar__logo">
-                       <img src={logo} alt="logo" />
-                    </NavLink>
-                    <button className="navbar__category-btn">
-                        <IoMenu />
-                        <span>Katalog</span>
-                    </button>
-                    <div className="navbar__search">
-                        <input type="text" placeholder='Search...' />
-                        <button><IoSearch /></button>
-                    </div>
-                    <div className="navbar__collection">
-                        <NavLink to={'/'} className="navbar__item">
-                            <IoHome />
-                            <span>Bosh Sahifa</span>
-                        </NavLink>
-                        <NavLink to={'/login'} className="navbar__item">
-                            <FaRegUser />
-                            <span>Account</span>
-                        </NavLink>
-                        <NavLink to={'/wishes'} className="navbar__item">
-                            <FaRegHeart />
-                            
-                            <span>Wishlist</span>
-                        </NavLink>
-                        <NavLink to={'/cart'} className="navbar__item">
-                            <FiShoppingCart />
-                            <span>Cart</span>
-                        </NavLink>
-                    </div>
-                </nav>
-            </div>
-        </header>
-    )
+              <span>Wishlist</span>
+            </NavLink>
+            <NavLink to={"/admin"} className="navbar__item">
+              <RiAdminLine />
+              <span>Admin</span>
+            </NavLink>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default memo(Navbar)
+export default memo(Navbar);
--- src/pages/single-route/SingleRoute.jsx
+++ src/pages/single-route/SingleRoute.jsx
@@ -21,87 +21,80 @@ const SingleRoute = () => {
   useEffect(() => {
     axios
       .get(`products/${id}`)
-      .then((res) => setProductData(res.data.products))
+      .then((res) => setProductData(res.data))
       .catch((res) => console.log(res));
   }, []);
 
-return (
-  <div className="singlePage">
-    <div className="container productAbout">
-      <div className="more_about_the_product">
-        <div id="product__about">
-          <div className="product_img">
-            <img src={product} alt="" />
-            {/* <img src={productData?.image} alt="" /> */}
-          </div>
-          <div className="product_desc">
-            <button className="smallBtn">Sale Off</button>
-            <h2 className="productName">
-              Seeds of Change Organic Quinoa, Brown
-            </h2>
-            <div className="comment">(32 reviews)</div>
-            <h4>
-              <span>$38</span>
-              <div className="chegirma">
-                26% Off
-                <p>$52</p>
-              </div>
-            </h4>
-            <p className="text">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam
-              rem officia, corrupti reiciendis minima nisi modi, quasi, odio
-              minus dolore impedit fuga eum eligendi.
-            </p>
-            <div className="product_div">
-              <div className="product_size">Size / Weight:</div>
-              <div className="singlePage_links">
-                <NavLink>50g</NavLink>
-                <NavLink className="activ">60g</NavLink>
-                <NavLink>80g</NavLink>
-                <NavLink>100g</NavLink>
-                <NavLink>150g</NavLink>
-              </div>
+  return (
+    <div className="singlePage">
+      <div className="container productAbout">
+        <div className="more_about_the_product">
+          <div id="product__about">
+            <div className="product_img">
+              <img src={productData?.images[0]} alt="" />
             </div>
+            <div className="product_desc">
+              <button className="smallBtn">Sale Off</button>
+              <h2 className="productName">{productData?.brand}</h2>
+              <div className="comment">(32 reviews)</div>
+              <h4>
+                <span>$38</span>
+                <div className="chegirma">
+                  {productData?.discountPercentage}% Off
+                  <p>$ {productData?.price}</p>
+                </div>
+              </h4>
+              <p className="text">{productData?.description}</p>
+              <div className="product_div">
+                <div className="product_size">Size / Weight:</div>
+                <div className="singlePage_links">
+                  <NavLink>50g</NavLink>
+                  <NavLink className="activ">60g</NavLink>
+                  <NavLink>80g</NavLink>
+                  <NavLink>100g</NavLink>
+                  <NavLink>150g</NavLink>
+                </div>
+              </div>
 
-            <div className="product_about_more">
-              <input type="number" name="number" id="number" value={1} />
-              <button className="card_add_btn">
-                <img src={savatcha} alt="savtcha" className="savatcha" />
-                Add to cart
-              </button>
-              <button className="like_btn">
-                <img src={like} alt="like" />
-              </button>
-              <button className="refresh_btn">
-                <img src={refresh} alt="refresh" />
-              </button>
+              <div className="product_about_more">
+                <input type="number" name="number" id="number"  />
+                <button className="card_add_btn">
+                  <img src={savatcha} alt="savtcha" className="savatcha" />
+                  Add to cart
+                </button>
+                <button className="like_btn">
+                  <img src={like} alt="like" />
+                </button>
+                <button className="refresh_btn">
+                  <img src={refresh} alt="refresh" />
+                </button>
+              </div>
             </div>
+          </div>
+
+          <div>
+            <h5>
+              <img src={mahsulotlar} alt="products" id="mahsulotlar" />
+            </h5>
+          </div>
+          <div className="card_text">
+            <img src={text} alt="desc" id="product_desc_text" />
           </div>
         </div>
-
         <div>
-          <h5>
-            <img src={mahsulotlar} alt="products" id="mahsulotlar" />
-          </h5>
-        </div>
-        <div className="card_text">
-          <img src={text} alt="desc" id="product_desc_text" />
+          <h4>
+            <img src={category} alt="category" id="category" />
+          </h4>
+          <h4>
+            <img src={price} alt="price" id="price" />
+          </h4>
+          <h4>
+            <img src={products} alt="products" id="product" />
+          </h4>
         </div>
       </div>
-      <div>
-        <h4>
-          <img src={category} alt="category" id="category" />
-        </h4>
-        <h4>
-          <img src={price} alt="price" id="price" />
-        </h4>
-        <h4>
-          <img src={products} alt="products" id="product" />
-        </h4>
-      </div>
+      <Wrapper />
     </div>
-    <Wrapper />
-  </div>
-);
-}
+  );
+};
 export default memo(SingleRoute);
